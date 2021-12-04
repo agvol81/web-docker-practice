34b6d9ef7d067342e71ed951e1ee6c7a6c3180d9
agvol <agvol81@gmail.com>
2021-12-05 03:17:59 +0600

    Практика по Docker

--- config/redis.js
+++ config/redis.js
@@ -32,7 +32,9 @@ module.exports = {
   |
   */
   local: {
-    host: '127.0.0.1',
+    //как понял параметры подключения к REDIS тут, прячем базу внутри сети докера
+    host: Env.get('REDIS_HOST', 'localhost'),
+    //host: '127.0.0.1',
     port: 6379,
     password: null,
     db: 0,
